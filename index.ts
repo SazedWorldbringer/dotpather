7cf42b682477b927bb2268d9b27776539eccaf35
Atharva Pardeshi <atharva.exe@gmail.com>
2023-01-22 11:16:10 +0530

    feat: update type to be an object with a string key and any value

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
-type FixMeLater = any
+type ObjectWithPath = {
+  [key: string]: any
+}
 
 export default function dotpath(str: string) {
   const parts = str.split('.')
   const len = parts.length
 
-  return function parse(obj: FixMeLater) {
+  return function parse(obj: ObjectWithPath) {
     let testKey
 
     for (let i = 0; i < len; ++i) {
